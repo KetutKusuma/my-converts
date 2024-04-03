dc0225c55f160cf03207599f11276991468f5cb0
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-04-03 11:45:32 +0800

    done bang

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import "time"
 
+// conver string to time with format : 2006-01-02 15:04:05
 func StrToTimeWFormat(str string) (time.Time, error) {
 	return time.Parse("2006-01-02 15:04:05", str)
 }
 
+// conver time to string with format : 2006-01-02 15:04:05
 func TimeToStrWFormat(time time.Time) string {
 	return time.Format("2006-01-02 15:04:05")
+}
+
+// conver string to time with format : 2006-01-02
+func StrToTimeWFormatDate(str string) (time.Time, error) {
+	return time.Parse("2006-01-02", str)
+}
+
+// conver time to string with format : 2006-01-02
+func TimeToStrWFormatDate(time time.Time) string {
+	return time.Format("2006-01-02")
 }
