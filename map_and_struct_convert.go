bcd406244a73a68012f744a2c344b96be0a94ac0
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-04-04 10:51:14 +0800

    done map converts

---
+++ map_and_struct_convert.go
@@ -0,0 +1,110 @@
+package myconverts
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type MapDynamic = map[string]interface{}
+
+// convert struct to map[string]interface{}
+func StructToMap(obj interface{}) (MapDynamic, error) {
+	var result MapDynamic
+
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(jsonBytes, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// convert struct to []byte{}
+func StructToByte(obj interface{}) ([]byte, error) {
+
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		fmt.Println("Marshal StructToByte.Err : ", err)
+
+		return nil, err
+	}
+
+	return jsonBytes, err
+}
+
+// convert struct + id (string) to []byte{}
+func StructAndIdToByte(obj interface{}, id string) ([]byte, error) {
+	/// ini untuk put biasanya
+	/// pergunakan sesuai kebutuhan ges
+	mapS := make(MapDynamic)
+	jsonBytes, err := json.Marshal(obj)
+	if err != nil {
+		fmt.Println("Marshal StructToByte.Err : ", err)
+
+		return nil, err
+	}
+	merr := json.Unmarshal(jsonBytes, &mapS)
+	if merr != nil {
+		return nil, merr
+	}
+
+	mapS["id"] = id
+	finalBytes, err := json.Marshal(mapS)
+
+	return finalBytes, err
+}
+
+// convert map[string]interface{} to []byte{}
+func MapToByte(theMap MapDynamic) ([]byte, error) {
+	finalBytes, err := json.Marshal(theMap)
+	if err != nil {
+		fmt.Println("Marshal MapToByte Err : ", err)
+	}
+
+	return finalBytes, err
+}
+
+// convert url.values to map[string]interface{}
+func UrlValuesToMap(urlV url.Values) (MapDynamic, error) {
+	if len(urlV) == 0 {
+		return nil, errors.New("url.values is empty")
+	}
+	resultMap := make(map[string]interface{})
+	for key, vals := range urlV {
+		if len(vals) == 1 {
+			resultMap[key] = vals[0]
+		} else {
+			var interfaceSlice []interface{}
+			for _, v := range vals {
+				interfaceSlice = append(interfaceSlice, v)
+			}
+			resultMap[key] = interfaceSlice
+		}
+	}
+	return resultMap, nil
+}
+
+func BodyResToMap(resp *http.Response) (map[string]interface{}, error) {
+	defer resp.Body.Close()
+	mapReturn := make(map[string]interface{})
+	parsedBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("io.ReadAll err : ", err)
+
+		return nil, err
+	}
+	mErr := json.Unmarshal(parsedBody, &mapReturn)
+	if mErr != nil {
+		fmt.Println("json.Unmarsal err : ", err)
+	}
+	return mapReturn, mErr
+}
