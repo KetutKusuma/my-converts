0911c3b404f0b12399c433e8fbf8cdd60a225cc5
Ketut Kusuma <71806737+KetutKusuma@users.noreply.github.com>
2024-04-23 13:13:12 +0800

    update dikit

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package myconverts
 
 import "time"
 
+/// BORHH
+
 // convert time to string with format : 2006-01-02 15:04:05
 func TimeToStrWFormat(time time.Time) string {
 	return time.Format("2006-01-02 15:04:05")
